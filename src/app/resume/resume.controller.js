6a95d18418b751904c364617cb80d18cdba7532d
loljoho <jho1290@smail.raritanval.edu>
2015-06-23 11:58:31 -0400

    Replaced md-menu with individual md-buttons due to 0.10.0 ngMaterial bugs

--- src/app/resume/resume.controller.js
+++ src/app/resume/resume.controller.js
@@ -6,28 +6,16 @@
     .controller('ResumeController', ResumeController);
 
   /** @ngInject */
-  function ResumeController($timeout, resumeService, toastr, $log) {
+  function ResumeController(resumeService) {
     var vm = this;
 
     vm.resumeData = [];
-    //vm.classAnimation = '';
-    //vm.creationDate = 1434909702629;
-    //vm.showToastr = showToastr;
 
     activate();
 
     function activate() {
       vm.resumeData = getResume();
-      $log.info(vm.resumeData);
-    //  $timeout(function() {
-    //    vm.classAnimation = 'rubberBand';
-    //  }, 4000);
     }
-
-    //function showToastr() {
-    //  toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    //  vm.classAnimation = '';
-    //}
 
     function getResume() {
       vm.resumeData = resumeService.get().$promise
