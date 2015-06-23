930ec644f326f8d9c6876b0ac52e732d080f7b4f
loljoho <jho1290@smail.raritanval.edu>
2015-06-23 08:26:47 -0400

    Sidenav left

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,7 +6,7 @@
     .config(config);
 
   /** @ngInject */
-  function config($mdThemingProvider, $logProvider, toastr) {
+  function config($mdThemingProvider, $mdIconProvider, $logProvider, toastr) {
     // Enable log
     $logProvider.debugEnabled(true);
   
@@ -14,6 +14,14 @@
       .primaryPalette('grey')
       .accentPalette('red')
       .warnPalette('deep-orange');
+
+    $mdIconProvider
+      .icon('github',         'assets/icons/github.svg'    )
+      .icon('linkedin',       'assets/icons/linkedin.svg'  )
+      .icon('angellist',      'assets/icons/angellist.svg' )
+      .icon('google',         'assets/icons/google.svg'    )
+      .icon('dropbox',        'assets/icons/dropbox.svg'   )
+      .icon('onedrive',       'assets/icons/onedrive.svg'  );
 
     // Set options third-party lib
     toastr.options.timeOut = 3000;
