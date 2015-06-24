d75fa1623c3068ea8e858af36296efb033a0c5a1
loljoho <jho1290@smail.raritanval.edu>
2015-06-24 02:27:57 -0400

    Restructuring application

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,12 +8,40 @@
   /** @ngInject */
   function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
+      /*
+       * HOME
+       *  > Summary
+       *  > Ongoing
+       *  > Profile
+       *  > Contact
+       */
       .state('home', {
         url: '/',
-        templateUrl: 'app/resume/resume.html',
+        templateUrl: 'app/resume/resume.html'
       })
-      .state('section', {
-        url: '/section',
+
+      /*
+       * SKILLS
+       *  > Solution Stack
+       *  > Areas of Expertise
+       *  > Developmental Focus
+       *  > Skills & Proficiencies
+       */
+      .state('skills', {
+        url: '/skills',
+        templateUrl: 'app/skills/skils.html',
+        controller: 'SkillsController',
+        controllerAs: 'sm'
+      })
+
+      /*
+       * SKILLS
+       *
+       *
+       *
+       */
+      .state('experience', {
+        url: '/experience',
         templateUrl: 'app/resume/resume.html',
         controller: 'ResumeController',
         controllerAs: 'sm'
---
+++ src/app/skills/skills.controller.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('resumeApp')
+    .controller('SkillsController', SkillsController);
+
+  /** @ngInject */
+  function SkillsController($mdSidenav) {
+    var self = this;
+
+    
+    vm.resumeData = [];
+
+    activate();
+
+    function activate() {
+      vm.resumeData = getResume();
+    }
+
+    function getResume() {
+      vm.resumeData = resumeService.get().$promise
+        .then(function(response) {
+          vm.resumeData = response;
+        }, function(errorMsg) {
+          vm.resumeData = errorMsg;
+        });
+      return vm.resumeData;
+    }
+  }
+})();
